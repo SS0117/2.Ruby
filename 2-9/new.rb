97a88d57f8bf5f8a3c8465e6bc494aedcc7f066d
SS0117 <103245744+SS0117@users.noreply.github.com>
2023-02-24 13:22:43 +0000

    [Finish]2-9

--- 2-9/new.rb
+++ 2-9/new.rb
@@ -15,13 +15,17 @@ class Car
   end
 end
 
-car = Car.new
-car.move("右", 5)
+# car = Car.new
+# car.move("右", 5)
 
 class Car
   def self.run(distance)
     puts "車で#{distance}キロ走ります。"
   end
+
+  def self.turn(direction)
+    puts "#{direction}に曲がります。"
+  end
 end
 
-Car.run(10)
+Car.turn("右")
